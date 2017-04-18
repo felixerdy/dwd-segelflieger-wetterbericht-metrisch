5e90bed6c4d9a234d92b07d45469008b5e775cd6
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-04-18 20:28:47 +0200

    Allow first value in ranges to have unit too

--- tests.js
+++ tests.js
@@ -68,6 +68,7 @@ describe('convertFeet()', function() {
     expect(convertFeet('1234FT')).to.equal('<code>400m</code>');
     expect(convertFeet('4200 FT')).to.equal('<code>1300m</code>');
     expect(convertFeet('1200 bis 4200 FT')).to.equal('<code>400 bis 1300m</code>');
+    expect(convertFeet('1200FT bis 4200 FT')).to.equal('<code>400 bis 1300m</code>');
     expect(convertFeet('1200    bis 4200FT')).to.equal('<code>400 bis 1300m</code>');
   });
 
@@ -82,6 +83,7 @@ describe('convertKnots()', function() {
     expect(convertKnots('12KT')).to.equal('<code>22km/h</code>');
     expect(convertKnots('42 KT')).to.equal('<code>78km/h</code>');
     expect(convertKnots('12 bis 40 KT')).to.equal('<code>22 bis 74km/h</code>');
+    expect(convertKnots('12KT bis 40 KT')).to.equal('<code>22 bis 74km/h</code>');
     expect(convertKnots('12 bis    40KT')).to.equal('<code>22 bis 74km/h</code>');
   });
 
